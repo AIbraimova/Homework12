c3a860d85aa24fbe6bc4016f4905f84ca8bc4ab5
Aiperi Ibraimova <aiperiibraimova@MacBook-Pro-von-Aiperi.local>
2022-11-03 13:52:06 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String args[]) {
+        Triangle size = new Triangle();
+        size.a = 5;
+        size.b = 5;
+        size.c = 5;
+        size.area();
+    }
+}
+
+
